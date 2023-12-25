92696408d5b4f6231d16a1973a4791c30affeed4
ArashNabaei <Arash_ANB@yahoo.com>
2023-12-25 16:08:31 +0330

    Install FluentValidation library to Application project Add validation for CreateAuthorCommand

---
+++ Application/Repositories/Command/Authors/Create/CreateAuthorCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Repositories.Command.Authors.Create
+{
+    public class CreateAuthorCommandValidator : AbstractValidator<CreateAuthorCommand>
+    {
+
+        public CreateAuthorCommandValidator()
+        {
+            RuleFor(p => p.Username)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("{PropertyName} must not be null.");
+
+            RuleFor(p => p.Password)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("{PropertyName} must not be null.");
+        }
+
+    }
+}
