b78cf7309788d2ed3c73c27c613390e191b9b781
ArashNabaei <Arash_ANB@yahoo.com>
2023-12-31 15:57:23 +0330

    Add validator for Delete Author

---
+++ Application/Repositories/Command/Authors/Delete/DeleteAuthorCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories.Command.Authors.Delete
+{
+    public class DeleteAuthorCommandValidator : AbstractValidator<DeleteAuthorCommand>
+    {
+        public DeleteAuthorCommandValidator() 
+        {
+            RuleFor(p => p.Id)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithMessage("{PropertyName} must be positive.");
+
+        }
+
+
+    }
+}
