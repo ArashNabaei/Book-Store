218b1fa1710edc6a4b3ebc8473e9cf2fa0166b29
ArashNabaei <Arash_ANB@yahoo.com>
2023-12-31 16:24:01 +0330

    Add validators for create, delete and update customer

---
+++ Application/Repositories/Command/Customers/Delete/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿
+
+using FluentValidation;
+
+namespace Application.Repositories.Command.Customers.Delete
+{
+    public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+    {
+
+        public DeleteCustomerCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithMessage("{PropertyName} must be positive");
+        }
+
+    }
+}
