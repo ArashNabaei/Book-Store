72254afdf4ae544ce4e83bedfa9019d50c7db2f0
ArashNabaei <Arash_ANB@yahoo.com>
2023-12-31 16:06:08 +0330

    Add validators for create, delete and update book

---
+++ Application/Repositories/Command/Books/Create/CreateBookCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿
+
+using FluentValidation;
+
+namespace Application.Repositories.Command.Books.Create
+{
+    public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
+    {
+
+        public CreateBookCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("{PropertyName} must not be null.");
+
+            RuleFor(p => p.Price)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithMessage("{PropertyName} must be positive.");
+
+            RuleFor(p => p.Genre)
+                .Cascade(CascadeMode.Stop)
+                .NotNull().WithMessage("{PropertyName} must not be null.");
+        }
+    }
+}
---
+++ Application/Repositories/Command/Books/Delete/DeleteBookCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories.Command.Books.Delete
+{
+    public class DeleteBookCommandValidator : AbstractValidator<DeleteBookCommand>
+    {
+        public DeleteBookCommandValidator()
+        {
+            RuleFor(p => p.Id)
+                .Cascade(CascadeMode.Stop)
+                .GreaterThan(0).WithMessage("{PropertyName} must be positive.");
+        }
+
+    }
+}
